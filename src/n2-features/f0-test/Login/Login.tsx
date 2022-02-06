3b12f10bf5e3a214acb7fc430148de3e5a211a9e
Yuliya <julia1997.04.14@mail.ru>
2022-02-06 16:38:22 +0300

    change JSX

--- src/n2-features/f0-test/Login/Login.tsx
+++ src/n2-features/f0-test/Login/Login.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 //first
 export const Login = (): React.ReactElement => (
-    <div>
-        <h1>LOGIN COMPONENT</h1>
+    <div className={'loginContainer'}>
+        <h1>Sign In</h1>
+        <div><input type={'email'} placeholder={'Email'}/></div>
+        <div><input type={'password'} placeholder={'Password'}/></div>
+        <div><span>Remember me</span><input type={'checkbox'}/></div>
+        <div><a href={''}>forgot?</a></div>
+        <div><button>Login</button></div>
+        <div><a href={''}>Don't have an account?</a></div>
+        <div><button>Sign Up</button></div>
     </div>
 );
