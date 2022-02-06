0dbdd1953fc7ae73f9852ade400dd16a65da37c2
Yuliya <julia1997.04.14@mail.ru>
2022-02-06 16:45:52 +0300

    add localState for login page

--- src/n2-features/f0-test/Login/Login.tsx
+++ src/n2-features/f0-test/Login/Login.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-//first
-export const Login = (): React.ReactElement => (
-    <div className={'loginContainer'}>
-        <h1>Sign In</h1>
-        <div><input type={'email'} placeholder={'Email'}/></div>
-        <div><input type={'password'} placeholder={'Password'}/></div>
-        <div><span>Remember me</span><input type={'checkbox'}/></div>
-        <div><a href={''}>forgot?</a></div>
-        <div><button>Login</button></div>
-        <div><a href={''}>Don't have an account?</a></div>
-        <div><button>Sign Up</button></div>
-    </div>
-);
+import React, {useState} from 'react';
+
+export const Login = (): React.ReactElement => {
+        const [email, setEmail] = useState('blabla@mail.ru')
+        const [password, setPassword] = useState('12345')
+
+        return  <div className={'loginContainer'}>
+                <h1>Sign In</h1>
+                <div><input type={'email'} placeholder={'Email'} value={email}/></div>
+                <div><input type={'password'} placeholder={'Password'} value={password}/></div>
+                <div><span>Remember me</span><input type={'checkbox'}/></div>
+                <div><a href={''}>forgot?</a></div>
+                <div><button>Login</button></div>
+                <div><a href={''}>Don't have an account?</a></div>
+                <div><button>Sign Up</button></div>
+        </div>
+}
