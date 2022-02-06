b5bf6e4467bc75562ebe0699aef35ba2aa034b11
Yuliya <julia1997.04.14@mail.ru>
2022-02-06 16:55:42 +0300

    create functions for changing state

--- src/n2-features/f0-test/Login/Login.tsx
+++ src/n2-features/f0-test/Login/Login.tsx
@@ -1,13 +1,18 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 export const Login = (): React.ReactElement => {
+        console.log('login')
+
         const [email, setEmail] = useState('blabla@mail.ru')
         const [password, setPassword] = useState('12345')
 
+        const changeEmail = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)
+        const changePassword = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)
+
         return  <div className={'loginContainer'}>
                 <h1>Sign In</h1>
-                <div><input type={'email'} placeholder={'Email'} value={email}/></div>
-                <div><input type={'password'} placeholder={'Password'} value={password}/></div>
+                <div><input type={'email'} placeholder={'Email'} value={email} onChange={changeEmail}/></div>
+                <div><input type={'password'} placeholder={'Password'} value={password} onChange={changePassword}/></div>
                 <div><span>Remember me</span><input type={'checkbox'}/></div>
                 <div><a href={''}>forgot?</a></div>
                 <div><button>Login</button></div>
