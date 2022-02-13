e141817d9537f0e821cbbea5737f4cafe040601c
Yuliya <julia1997.04.14@mail.ru>
2022-02-13 23:06:25 +0300

    add onKeyPressHandler and some changes for saveHandler

--- src/n1-main/m1-ui/common/AddItemForm/AddItem.tsx
+++ src/n1-main/m1-ui/common/AddItemForm/AddItem.tsx
@@ -1,10 +1,9 @@
 import SuperButton from "../c2-SuperButton/SuperButton";
 import SuperInputText from "../c1-SuperInputText/SuperInputText";
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, KeyboardEvent, useState} from "react";
 import {PacksList} from "../../../../n2-features/f1-table/Packs/PacksList";
 import {AddCardPacksThunk} from "../../../../n2-features/f1-table/Packs/PacksReducer";
 import {useDispatch} from "react-redux";
-import {generateKeyPair} from "crypto";
 
 type AddItemPropsType = {
     title: string
@@ -14,12 +13,30 @@ export const AddItem = (props: AddItemPropsType) => {
 
     const [addPack, setAddPack] = useState<boolean>(true)
     const [title, setTitle] = useState<string>('')
+    const [error, setError] = useState<string | null>(null)
 
     const cancelHandler = () => setAddPack(false)
     const onChangeTitleHandler = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
     const saveHandler = () => {
-        dispatch(AddCardPacksThunk(title))
-        setAddPack(false)
+        if (title.trim() !== '') {
+            dispatch(AddCardPacksThunk(title))
+            setAddPack(false)
+            setTitle('')
+        } else if (title.trim() === ''){
+            setError('Field is required!')
+            if (error !== null) setError(null)
+        }
+    }
+    const onKeyPressHandler = (e:KeyboardEvent<HTMLInputElement>) => {
+        if(error !== null) setError(null)
+        if(e.charCode === 13 && title.trim() !== ''){
+            dispatch(AddCardPacksThunk(title))
+            setAddPack(false)
+            setTitle('')
+        } else if (e.charCode === 13 && title.trim() === ''){
+            setError('Field is required!')
+            if (error !== null) setError(null)
+        }
     }
 
     return <div>
@@ -30,7 +47,10 @@ export const AddItem = (props: AddItemPropsType) => {
                     <SuperButton>X</SuperButton>
                 </div>
                 <SuperInputText placeholder={'Title pack'} value = {title}
-                                onChange={onChangeTitleHandler}/>
+                                onChange={onChangeTitleHandler}
+                                onKeyPress={onKeyPressHandler}
+                />
+                {error && <div>{error}</div>}
                 <div>
                     <SuperButton onClick={cancelHandler}>Cancel</SuperButton>
                     <SuperButton onClick={saveHandler}>Save</SuperButton>
